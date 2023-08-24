a379f7d994354917b6543506683e5cb81c25355d
andrew-novak <contact@andrewnovak.co.uk>
2023-08-24 20:17:00 +0100

    client's socket.emit wrapped in try catch

--- clientWeb/src/actions/send.js
+++ clientWeb/src/actions/send.js
@@ -2,9 +2,20 @@ import { API_URL } from "constants/urls";
 
 export const sendText = (socket, author, content) => (dispatch) => {
   if (content.length < 1) return;
-  socket.emit("text message", { author, content });
+  try {
+    socket.emit("text message", { author, content });
+  } catch (err) {
+    console.error("An error occured during a text message send attempt:", err);
+  }
 };
 
 export const sendEmoji = (socket, author, emojiName) => (dispatch) => {
-  socket.emit("emoji message", { author, emojiName });
+  try {
+    socket.emit("emoji message", { author, emojiName });
+  } catch (err) {
+    console.error(
+      "An error occured during an emoji message send attempt:",
+      err
+    );
+  }
 };
